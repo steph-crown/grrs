ef2f3a3c94c365f1a887a1f079142447124eea1e
Steph Crown <emmanuelstephen024@gmail.com>
2025-12-15 23:53:40 +0100

    retrieve command-line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+  let pattern = get_arg(1, "No pattern is given");
+  let path = get_arg(2, "No path is given");
+
+  println!("pattern {:?}, path {:?}", pattern, path);
+}
+
+fn get_arg(pos: usize, err_message: &'static str) -> String {
+  let value = args().nth(pos).expect(err_message);
+
+  value
 }
