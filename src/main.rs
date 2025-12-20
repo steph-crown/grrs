7c936895c32bda20a7a1322e04c6f877f7963580
Steph Crown <emmanuelstephen024@gmail.com>
2025-12-20 14:15:14 +0100

    Add update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
-use clap::Parser;
-use std::{fs::read_to_string, path::PathBuf};
+use clap::{Error, Parser};
+use std::{
+  fs::{File, read_to_string},
+  io::{BufRead, BufReader},
+  path::PathBuf,
+};
 
 /// Search for a pattern in a file and display the lines that contain it
 #[derive(Parser)]
@@ -9,13 +13,47 @@ struct Cli {
   path: PathBuf,
 }
 
-fn main() {
-  let args = Cli::parse();
-  let content = read_to_string(&args.path).expect("An error occured readding file");
+#[derive(Debug)]
+struct CustomError(String);
 
-  for (index, line) in content.lines().enumerate() {
-    if line.contains(&args.pattern) {
-      println!("{}: {line}", index + 1)
+fn main() -> Result<(), CustomError> {
+  let args = Cli::parse();
+
+  return process_args_v2(&args);
+}
+
+// fn process_args_v1(args: &Cli) {
+//   let content = read_to_string(&args.path).expect("An error occured readding file");
+
+//   for (index, line) in content.lines().enumerate() {
+//     if line.contains(&args.pattern) {
+//       println!("{}: {line}", index + 1)
+//     }
+//   }
+// }
+
+fn process_args_v2(args: &Cli) -> Result<(), CustomError> {
+  let file = File::open(&args.path).map_err(|err| {
+    CustomError(format!(
+      "Error reading file at path `{:?}`: {}",
+      args.path, err
+    ))
+  })?;
+
+  // let file_content = match file {
+  //   Ok(content) => content,
+  //   Err(err) => return Err(err.into()),
+  // };
+
+  let reader = BufReader::new(&file);
+
+  for (index, line) in reader.lines().enumerate() {
+    let line_content = line.expect("Error reading line");
+
+    if line_content.contains(&args.pattern) {
+      println!("{}: {}", index + 1, line_content)
     }
   }
+
+  Ok(())
 }
